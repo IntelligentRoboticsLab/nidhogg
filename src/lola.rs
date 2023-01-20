d84eba618108e6a60c34d0b8db9ec048201c3831
Harold Ruiter <haroldruiter28@gmail.com>
2023-01-20 12:01:30 +0100

    Make private from/into methods

--- src/lola.rs
+++ src/lola.rs
@@ -1,8 +1,48 @@
 use crate::types::*;
 use serde::{Deserialize, Serialize};
 
-impl From<Ear<Left>> for [f32; 10] {
-    fn from(value: Ear<Left>) -> Self {
+/// A trait that provides conversions from Nidhogg data to LoLA data
+/// ## 🗒️ Note:
+/// Like [`From`] does with [`Into`], this trait does automatically provides an implementation for [`IntoLoLA`].
+trait FromNidhogg<N> {
+    fn from_nidhogg(value: N) -> Self;
+}
+
+/// A trait that provides conversions from Nidhogg data to LoLA data
+/// ## ⚠️ Warning:
+// This trait gets automatically implemented when implementing [`FromNidhogg`], so you should prefer implementing that.
+trait IntoLoLA<L> {
+    fn into_lola(self) -> L;
+}
+
+impl<N, L: FromNidhogg<N>> IntoLoLA<L> for N {
+    fn into_lola(self) -> L {
+        L::from_nidhogg(self)
+    }
+}
+
+/// A trait that provides conversions from LoLA data to Nidhogg data
+/// ## 🗒️ Note:
+/// Like [`From`] does with [`Into`], this trait does automatically provides an implementation for [`IntoLoLA`].
+trait FromLoLA<L> {
+    fn from_lola(value: L) -> Self;
+}
+
+/// A trait that provides conversions from LoLA data to Nidhogg data
+/// ## ⚠️ Warning:
+// This trait gets automatically implemented when implementing [`FromNidhogg`], so you should prefer implementing that.
+trait IntoNidhogg<N> {
+    fn into_nidhogg(self) -> N;
+}
+
+impl<L, N: FromLoLA<L>> IntoNidhogg<N> for L {
+    fn into_nidhogg(self) -> N {
+        N::from_lola(self)
+    }
+}
+
+impl FromNidhogg<Ear<Left>> for [f32; 10] {
+    fn from_nidhogg(value: Ear<Left>) -> Self {
         [
             value.intensity_0_deg,
             value.intensity_36_deg,
@@ -18,8 +58,8 @@ impl From<Ear<Left>> for [f32; 10] {
     }
 }
 
-impl From<Ear<Right>> for [f32; 10] {
-    fn from(value: Ear<Right>) -> Self {
+impl FromNidhogg<Ear<Right>> for [f32; 10] {
+    fn from_nidhogg(value: Ear<Right>) -> Self {
         [
             value.intensity_324_deg,
             value.intensity_288_deg,
@@ -35,14 +75,14 @@ impl From<Ear<Right>> for [f32; 10] {
     }
 }
 
-impl From<Color> for [f32; 3] {
-    fn from(value: Color) -> Self {
+impl FromNidhogg<Color> for [f32; 3] {
+    fn from_nidhogg(value: Color) -> Self {
         [value.red, value.green, value.blue]
     }
 }
 
-impl From<Eye<Left>> for [f32; 24] {
-    fn from(value: Eye<Left>) -> Self {
+impl FromNidhogg<Eye<Left>> for [f32; 24] {
+    fn from_nidhogg(value: Eye<Left>) -> Self {
         [
             value.color_45_deg.red,
             value.color_0_deg.red,
@@ -74,8 +114,8 @@ impl From<Eye<Left>> for [f32; 24] {
     }
 }
 
-impl From<Eye<Right>> for [f32; 24] {
-    fn from(value: Eye<Right>) -> Self {
+impl FromNidhogg<Eye<Right>> for [f32; 24] {
+    fn from_nidhogg(value: Eye<Right>) -> Self {
         [
             value.color_0_deg.red,
             value.color_45_deg.red,
@@ -107,8 +147,8 @@ impl From<Eye<Right>> for [f32; 24] {
     }
 }
 
-impl From<Skull> for [f32; 12] {
-    fn from(value: Skull) -> Self {
+impl FromNidhogg<Skull> for [f32; 12] {
+    fn from_nidhogg(value: Skull) -> Self {
         [
             value.left_front_0,
             value.left_front_1,
@@ -126,43 +166,49 @@ impl From<Skull> for [f32; 12] {
     }
 }
 
-impl<T: Copy> From<[T; 25]> for JointArray<T> {
-    fn from(value: [T; 25]) -> Self {
+impl<T> FromLoLA<[T; 25]> for JointArray<T> {
+    fn from_lola(value: [T; 25]) -> Self {
+        let [head_yaw, head_pitch, left_shoulder_pitch, left_shoulder_roll, left_elbow_yaw, // bad rustfmt
+             left_elbow_roll, left_wrist_yaw, left_hip_yaw_pitch, left_hip_roll, left_hip_pitch,
+             left_knee_pitch, left_ankle_pitch, left_ankle_roll, right_hip_roll, right_hip_pitch,
+             right_knee_pitch, right_ankle_pitch, right_ankle_roll, right_shoulder_pitch, right_shoulder_roll,
+             right_elbow_yaw, right_elbow_roll, right_wrist_yaw, left_hand, right_hand] = value;
+
         Self {
-            head_yaw: value[0],
-            head_pitch: value[1],
+            head_yaw,
+            head_pitch,
 
-            left_shoulder_pitch: value[2],
-            left_shoulder_roll: value[3],
-            left_elbow_yaw: value[4],
-            left_elbow_roll: value[5],
-            left_wrist_yaw: value[6],
-            left_hip_yaw_pitch: value[7],
-            left_hip_roll: value[8],
-            left_hip_pitch: value[9],
-            left_knee_pitch: value[10],
-            left_ankle_pitch: value[11],
-            left_ankle_roll: value[12],
+            left_shoulder_pitch,
+            left_shoulder_roll,
+            left_elbow_yaw,
+            left_elbow_roll,
+            left_wrist_yaw,
+            left_hip_yaw_pitch,
+            left_hip_roll,
+            left_hip_pitch,
+            left_knee_pitch,
+            left_ankle_pitch,
+            left_ankle_roll,
 
-            right_hip_roll: value[13],
-            right_hip_pitch: value[14],
-            right_knee_pitch: value[15],
-            right_ankle_pitch: value[16],
-            right_ankle_roll: value[17],
-            right_shoulder_pitch: value[18],
-            right_shoulder_roll: value[19],
-            right_elbow_yaw: value[20],
-            right_elbow_roll: value[21],
-            right_wrist_yaw: value[22],
+            right_hip_roll,
+            right_hip_pitch,
+            right_knee_pitch,
+            right_ankle_pitch,
+            right_ankle_roll,
+            right_shoulder_pitch,
+            right_shoulder_roll,
+            right_elbow_yaw,
+            right_elbow_roll,
+            right_wrist_yaw,
 
-            left_hand: value[23],
-            right_hand: value[24],
+            left_hand,
+            right_hand,
         }
     }
 }
 
-impl<T> From<JointArray<T>> for [T; 25] {
-    fn from(value: JointArray<T>) -> Self {
+impl<T> FromNidhogg<JointArray<T>> for [T; 25] {
+    fn from_nidhogg(value: JointArray<T>) -> Self {
         [
             value.head_yaw,
             value.head_pitch,
@@ -193,8 +239,8 @@ impl<T> From<JointArray<T>> for [T; 25] {
     }
 }
 
-impl From<[f32; 4]> for Battery {
-    fn from(value: [f32; 4]) -> Self {
+impl FromLoLA<[f32; 4]> for Battery {
+    fn from_lola(value: [f32; 4]) -> Self {
         Battery {
             charge: value[0],
             current: value[1],
@@ -204,20 +250,20 @@ impl From<[f32; 4]> for Battery {
     }
 }
 
-impl From<[f32; 8]> for ForceSensitiveResistors {
-    fn from(value: [f32; 8]) -> Self {
+impl FromLoLA<[f32; 8]> for ForceSensitiveResistors {
+    fn from_lola(value: [f32; 8]) -> Self {
         let left: [f32; 4] = value[..4].try_into().unwrap();
         let right: [f32; 4] = value[4..].try_into().unwrap();
 
         Self {
-            left_foot: left.into(),
-            right_foot: right.into(),
+            left_foot: left.into_nidhogg(),
+            right_foot: right.into_nidhogg(),
         }
     }
 }
 
-impl From<[f32; 4]> for ForceSensitiveResistorFoot {
-    fn from(value: [f32; 4]) -> Self {
+impl FromLoLA<[f32; 4]> for ForceSensitiveResistorFoot {
+    fn from_lola(value: [f32; 4]) -> Self {
         Self {
             front_left: value[0],
             front_right: value[1],
@@ -227,23 +273,21 @@ impl From<[f32; 4]> for ForceSensitiveResistorFoot {
     }
 }
 
-impl<T: Copy> From<[T; 2]> for Sonar<T> {
-    fn from(value: [T; 2]) -> Self {
-        Sonar {
-            left: value[0],
-            right: value[1],
-        }
+impl<T> FromLoLA<[T; 2]> for Sonar<T> {
+    fn from_lola(value: [T; 2]) -> Self {
+        let [left, right] = value;
+        Sonar { left, right }
     }
 }
 
-impl<T> From<Sonar<T>> for [T; 2] {
-    fn from(value: Sonar<T>) -> Self {
+impl<T> FromNidhogg<Sonar<T>> for [T; 2] {
+    fn from_nidhogg(value: Sonar<T>) -> Self {
         [value.left, value.right]
     }
 }
 
-impl From<[f32; 14]> for Touch {
-    fn from(value: [f32; 14]) -> Self {
+impl FromLoLA<[f32; 14]> for Touch {
+    fn from_lola(value: [f32; 14]) -> Self {
         Self {
             chest_board: value[0],
             head_front: value[1],
@@ -263,8 +307,8 @@ impl From<[f32; 14]> for Touch {
     }
 }
 
-impl From<[f32; 2]> for Vector2<f32> {
-    fn from(value: [f32; 2]) -> Self {
+impl FromLoLA<[f32; 2]> for Vector2<f32> {
+    fn from_lola(value: [f32; 2]) -> Self {
         Vector2 {
             x: value[0],
             y: value[1],
@@ -272,8 +316,8 @@ impl From<[f32; 2]> for Vector2<f32> {
     }
 }
 
-impl From<[f32; 3]> for Vector3<f32> {
-    fn from(value: [f32; 3]) -> Self {
+impl FromLoLA<[f32; 3]> for Vector3<f32> {
+    fn from_lola(value: [f32; 3]) -> Self {
         Vector3 {
             x: value[0],
             y: value[1],
@@ -304,18 +348,18 @@ pub struct RawState<'a> {
 impl From<RawState<'_>> for State {
     fn from(value: RawState) -> Self {
         Self {
-            stiffness: value.stiffness.into(),
-            position: value.position.into(),
-            temperature: value.temperature.into(),
-            current: value.current.into(),
-            battery: value.battery.into(),
-            accelerometer: value.accelerometer.into(),
-            gyroscope: value.gyroscope.into(),
-            angles: value.angles.into(),
-            sonar: value.sonar.into(),
-            force_sensitive_resistors: value.f_s_r.into(),
-            touch: value.touch.into(),
-            status: value.status.into(),
+            stiffness: value.stiffness.into_nidhogg(),
+            position: value.position.into_nidhogg(),
+            temperature: value.temperature.into_nidhogg(),
+            current: value.current.into_nidhogg(),
+            battery: value.battery.into_nidhogg(),
+            accelerometer: value.accelerometer.into_nidhogg(),
+            gyroscope: value.gyroscope.into_nidhogg(),
+            angles: value.angles.into_nidhogg(),
+            sonar: value.sonar.into_nidhogg(),
+            force_sensitive_resistors: value.f_s_r.into_nidhogg(),
+            touch: value.touch.into_nidhogg(),
+            status: value.status.into_nidhogg(),
         }
     }
 }
@@ -350,17 +394,17 @@ pub struct RawUpdate {
 impl From<Update> for RawUpdate {
     fn from(value: Update) -> Self {
         Self {
-            position: value.position.into(),
-            stiffness: value.stiffness.into(),
-            r_ear: value.right_ear.into(),
-            l_ear: value.left_ear.into(),
-            chest: value.chest.into(),
-            l_eye: value.left_eye.into(),
-            r_eye: value.right_eye.into(),
-            l_foot: value.left_foot.into(),
-            r_foot: value.right_foot.into(),
-            skull: value.skull.into(),
-            sonar: value.sonar.into(),
+            position: value.position.into_lola(),
+            stiffness: value.stiffness.into_lola(),
+            r_ear: value.right_ear.into_lola(),
+            l_ear: value.left_ear.into_lola(),
+            chest: value.chest.into_lola(),
+            l_eye: value.left_eye.into_lola(),
+            r_eye: value.right_eye.into_lola(),
+            l_foot: value.left_foot.into_lola(),
+            r_foot: value.right_foot.into_lola(),
+            skull: value.skull.into_lola(),
+            sonar: value.sonar.into_lola(),
         }
     }
 }
