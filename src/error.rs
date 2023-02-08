9cbb34635ecefde56c7c43100b71c2fcf421c272
Harold Ruiter <haroldruiter28@gmail.com>
2023-02-08 10:31:05 +0100

    Tag `Error` enum as `non_exhaustive`

--- src/error.rs
+++ src/error.rs
@@ -3,6 +3,7 @@ use thiserror::Error;
 pub type Result<T> = std::result::Result<T, Error>;
 
 #[derive(Error, Debug)]
+#[non_exhaustive]
 pub enum Error {
     #[error("Could not connect to LoLA socket")]
     NoLoLAConnection,
