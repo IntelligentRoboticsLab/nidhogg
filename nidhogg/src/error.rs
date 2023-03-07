48e5779e27bdb91454aade8e2acf405de388480b
Gijs de Jong <berichtaangijs@gmail.com>
2023-03-07 23:02:38 +0100

    Clearer wording

--- nidhogg/src/error.rs
+++ nidhogg/src/error.rs
@@ -9,7 +9,7 @@ pub enum Error {
     #[cfg(feature = "lola")]
     #[error("Could not connect to LoLA socket")]
     #[diagnostic(help("- Are you trying to connect to the simulation? This backend only supports real NAOs!
-- Are you running the code locally? Connecting with LoLA only works when ran from a NAO!
+- Are you running the code locally? Connecting with LoLA only works when ran on a NAO!
 - Are you using `LoLABackend::connect_with_retry` instead of `LoLABackend::connect`? You might not always get a connection the first time!"))]
     NoLoLAConnection(#[from] std::io::Error),
 
@@ -21,8 +21,7 @@ pub enum Error {
     #[error("Failed to encode MessagePack message")]
     MsgPackEncodeError(#[from] rmp_serde::encode::Error),
 
-    // todo: fork zmq and impl Display for `zmq_remote_api::RemoteApiError` so we can make it
-    // todo: transparant
+    // todo: fork zmq and impl Display for `zmq_remote_api::RemoteApiError` so we can make it transparant.
     #[cfg(feature = "coppelia")]
     #[error("Failed to connect to Coppelia simulator!")]
     CoppeliaConnectError(String),
