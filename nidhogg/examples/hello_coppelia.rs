e489239056504557e821786f49800514c93eb2e3
StinderdaadUvA <stin.thielen@student.uva.nl>
2023-03-22 15:15:19 +0100

    losse functies get_joint_handles en get_object

--- nidhogg/examples/hello_coppelia.rs
+++ nidhogg/examples/hello_coppelia.rs
@@ -1,8 +1,6 @@
-use nidhogg::{
-    backends::CoppeliaBackend,
-    types::{JointArray},
-    NaoBackend, NaoControlMessage,
-};
+use std::time::Duration;
+
+use nidhogg::{backends::CoppeliaBackend, types::JointArray, NaoBackend, NaoControlMessage};
 
 use miette::Result;
 
@@ -12,14 +10,42 @@ fn main() -> Result<()> {
     let mut nao = CoppeliaBackend::connect()?;
 
     let update = NaoControlMessage::builder()
+        .stiffness(JointArray {
+            head_yaw: 1.0,
+            head_pitch: 1.0,
+            left_shoulder_pitch: 1.0,
+            left_shoulder_roll: 1.0,
+            left_elbow_yaw: 1.0,
+            left_elbow_roll: 1.0,
+            left_wrist_yaw: 1.0,
+            left_hip_yaw_pitch: 1.0,
+            left_hip_roll: 1.0,
+            left_hip_pitch: 1.0,
+            left_knee_pitch: 1.0,
+            left_ankle_pitch: 1.0,
+            left_ankle_roll: 1.0,
+            right_hip_roll: 1.0,
+            right_hip_pitch: 1.0,
+            right_knee_pitch: 1.0,
+            right_ankle_pitch: 1.0,
+            right_ankle_roll: 1.0,
+            right_shoulder_pitch: 1.0,
+            right_shoulder_roll: 1.0,
+            right_elbow_yaw: 1.0,
+            right_elbow_roll: 1.0,
+            right_wrist_yaw: 1.0,
+            left_hand: 1.0,
+            right_hand: 1.0,
+        })
         .position(
-            JointArray::<f32>::builder().head_yaw(std::f32::consts::FRAC_PI_4).build()
+            JointArray::<f32>::builder()
+                .head_yaw(std::f32::consts::FRAC_PI_4)
+                .build(),
         )
         .build();
 
     loop {
         nao.send_control_msg(update.clone())?;
+        std::thread::sleep(Duration::from_millis(300));
     }
-
-    Ok(())
 }
--- nidhogg/src/backends/coppelia.rs
+++ nidhogg/src/backends/coppelia.rs
@@ -1,13 +1,13 @@
-use crate::{Error, NaoBackend, NaoControlMessage, NaoState, Result, types::JointArray};
-use zmq_remote_api::{sim::Sim, RemoteApiClient, RemoteApiClientParams};
+use crate::{types::JointArray, Error, NaoBackend, NaoControlMessage, NaoState, Result};
 use std::rc::Rc;
+use zmq_remote_api::{sim::Sim, RemoteApiClient, RemoteApiClientParams};
 
 #[allow(missing_debug_implementations)]
 pub struct CoppeliaBackend {
     #[allow(dead_code)]
     client: Rc<RemoteApiClient>,
     sim: Sim,
-    joint_handles: JointArray<i64>,
+    joint_handles: JointArray<i64>, //Option?
 }
 
 impl NaoBackend for CoppeliaBackend {
@@ -16,110 +16,204 @@ impl NaoBackend for CoppeliaBackend {
             RemoteApiClient::new(RemoteApiClientParams {
                 host: "localhost".to_string(),
                 ..RemoteApiClientParams::default()
-            }).map_err(|e| Error::CoppeliaConnectError(e.show()))?
+            })
+            .map_err(|e| Error::CoppeliaConnectError(e.show()))?,
         );
 
         let sim = Sim::new(client.clone());
-        // Get the joints
-        // TODO: make function that wraps sim.get_object() and handles the error properly
-        //  .map_err(|e| Error::CoppeliaConnectError(e.show()))?);
-        let joint_handles: JointArray<i64> = JointArray {
-            head_yaw: sim.get_object("/NAO/HeadYaw".to_string(), None).unwrap(),
-            head_pitch: sim.get_object("/NAO/HeadPitch".to_string(), None).unwrap(),
-
-            left_shoulder_pitch: sim.get_object("/NAO/LShoulderPitch".to_string(), None).unwrap(),
-            left_shoulder_roll: sim.get_object("/NAO/LShoulderRoll".to_string(), None).unwrap(),
-            left_elbow_yaw: sim.get_object("/NAO/LElbowYaw".to_string(), None).unwrap(),
-            left_elbow_roll: sim.get_object("/NAO/LElbowRoll".to_string(), None).unwrap(),
-            left_wrist_yaw: sim.get_object("/NAO/LWristYaw".to_string(), None).unwrap(),
-            left_hip_yaw_pitch: sim.get_object("/NAO/LHipYawPitch".to_string(), None).unwrap(),
-            left_hip_roll: sim.get_object("/NAO/LHipRoll".to_string(), None).unwrap(),
-            left_hip_pitch: sim.get_object("/NAO/LHipPitch".to_string(), None).unwrap(),
-            left_knee_pitch: sim.get_object("/NAO/LKneePitch".to_string(), None).unwrap(),
-            left_ankle_pitch: sim.get_object("/NAO/LAnklePitch".to_string(), None).unwrap(),
-            left_ankle_roll: sim.get_object("/NAO/LAnkleRoll".to_string(), None).unwrap(),
-
-            right_shoulder_pitch: sim.get_object("/NAO/RShoulderPitch".to_string(), None).unwrap(),
-            right_shoulder_roll: sim.get_object("/NAO/RShoulderRoll".to_string(), None).unwrap(),
-            right_elbow_yaw: sim.get_object("/NAO/RElbowYaw".to_string(), None).unwrap(),
-            right_elbow_roll: sim.get_object("/NAO/RElbowRoll".to_string(), None).unwrap(),
-            right_wrist_yaw: sim.get_object("/NAO/RWristYaw".to_string(), None).unwrap(),
-            // right_hip_yaw_pitch: sim.get_object("/NAO/RHipYawPitch".to_string(), None)?; // heeft LoLa niet,
-            right_hip_roll: sim.get_object("/NAO/RHipRoll".to_string(), None).unwrap(),
-            right_hip_pitch: sim.get_object("/NAO/RHipPitch".to_string(), None).unwrap(),
-            right_knee_pitch: sim.get_object("/NAO/RKneePitch".to_string(), None).unwrap(),
-            right_ankle_pitch: sim.get_object("/NAO/RAnklePitch".to_string(), None).unwrap(),
-            right_ankle_roll: sim.get_object("/NAO/RAnkleRoll".to_string(), None).unwrap(),
-
-            // handen heeft coppelia niet dus nog een keer wrist als placeholder
-            left_hand: sim.get_object("/NAO/LWristYaw".to_string(), None).unwrap(),
-            right_hand: sim.get_object("/NAO/RWristYaw".to_string(), None).unwrap(),
-        };
-
+        let joint_handles = get_joint_handles(&sim)?;
         client.to_owned().set_stepping(true).unwrap();
         sim.start_simulation().unwrap();
-        Ok(CoppeliaBackend { client, sim, joint_handles })
+        // let joint_handles = get_joint_handles(&sim);
+        Ok(CoppeliaBackend {
+            client,
+            sim,
+            joint_handles,
+        })
     }
 
-    fn send_control_msg(
-        &mut self,
-        update: NaoControlMessage,
-    ) -> Result<()> {
+    fn send_control_msg(&mut self, update: NaoControlMessage) -> Result<()> {
         self.set_joint_position(self.joint_handles.head_pitch, update.position.head_pitch)?;
         self.set_joint_position(self.joint_handles.head_yaw, update.position.head_yaw)?;
 
-        self.set_joint_position(self.joint_handles.left_shoulder_pitch, update.position.left_shoulder_pitch)?;
-        self.set_joint_position(self.joint_handles.left_shoulder_roll, update.position.left_shoulder_roll)?;
-        self.set_joint_position(self.joint_handles.left_elbow_yaw, update.position.left_elbow_yaw)?;
-        self.set_joint_position(self.joint_handles.left_elbow_roll, update.position.left_elbow_roll)?;
-        self.set_joint_position(self.joint_handles.left_wrist_yaw, update.position.left_wrist_yaw)?;
-        self.set_joint_position(self.joint_handles.left_hip_yaw_pitch, update.position.left_hip_yaw_pitch)?;
-        self.set_joint_position(self.joint_handles.left_hip_roll, update.position.left_hip_roll)?;
-        self.set_joint_position(self.joint_handles.left_hip_pitch, update.position.left_hip_pitch)?;
-        self.set_joint_position(self.joint_handles.left_knee_pitch, update.position.left_knee_pitch)?;
-        self.set_joint_position(self.joint_handles.left_ankle_pitch, update.position.left_ankle_pitch)?;
-        self.set_joint_position(self.joint_handles.left_ankle_roll, update.position.left_ankle_roll)?;
+        self.set_joint_position(
+            self.joint_handles.left_shoulder_pitch,
+            update.position.left_shoulder_pitch,
+        )?;
+        self.set_joint_position(
+            self.joint_handles.left_shoulder_roll,
+            update.position.left_shoulder_roll,
+        )?;
+        self.set_joint_position(
+            self.joint_handles.left_elbow_yaw,
+            update.position.left_elbow_yaw,
+        )?;
+        self.set_joint_position(
+            self.joint_handles.left_elbow_roll,
+            update.position.left_elbow_roll,
+        )?;
+        self.set_joint_position(
+            self.joint_handles.left_wrist_yaw,
+            update.position.left_wrist_yaw,
+        )?;
+        self.set_joint_position(
+            self.joint_handles.left_hip_yaw_pitch,
+            update.position.left_hip_yaw_pitch,
+        )?;
+        self.set_joint_position(
+            self.joint_handles.left_hip_roll,
+            update.position.left_hip_roll,
+        )?;
+        self.set_joint_position(
+            self.joint_handles.left_hip_pitch,
+            update.position.left_hip_pitch,
+        )?;
+        self.set_joint_position(
+            self.joint_handles.left_knee_pitch,
+            update.position.left_knee_pitch,
+        )?;
+        self.set_joint_position(
+            self.joint_handles.left_ankle_pitch,
+            update.position.left_ankle_pitch,
+        )?;
+        self.set_joint_position(
+            self.joint_handles.left_ankle_roll,
+            update.position.left_ankle_roll,
+        )?;
 
-        self.set_joint_position(self.joint_handles.right_shoulder_pitch, update.position.right_shoulder_pitch)?;
-        self.set_joint_position(self.joint_handles.right_shoulder_roll, update.position.right_shoulder_roll)?;
-        self.set_joint_position(self.joint_handles.right_elbow_yaw, update.position.right_elbow_yaw)?;
-        self.set_joint_position(self.joint_handles.right_elbow_roll, update.position.right_elbow_roll)?;
-        self.set_joint_position(self.joint_handles.right_wrist_yaw, update.position.right_wrist_yaw)?;
+        self.set_joint_position(
+            self.joint_handles.right_shoulder_pitch,
+            update.position.right_shoulder_pitch,
+        )?;
+        self.set_joint_position(
+            self.joint_handles.right_shoulder_roll,
+            update.position.right_shoulder_roll,
+        )?;
+        self.set_joint_position(
+            self.joint_handles.right_elbow_yaw,
+            update.position.right_elbow_yaw,
+        )?;
+        self.set_joint_position(
+            self.joint_handles.right_elbow_roll,
+            update.position.right_elbow_roll,
+        )?;
+        self.set_joint_position(
+            self.joint_handles.right_wrist_yaw,
+            update.position.right_wrist_yaw,
+        )?;
         // self.set_joint_position(self.joint_handles.right_hip_yaw_pitch, update.position.left_hip_yaw_pitch)?;
-        self.set_joint_position(self.joint_handles.right_hip_roll, update.position.right_hip_roll)?;
-        self.set_joint_position(self.joint_handles.right_hip_pitch, update.position.right_hip_pitch)?;
-        self.set_joint_position(self.joint_handles.right_knee_pitch, update.position.right_knee_pitch)?;
-        self.set_joint_position(self.joint_handles.right_ankle_pitch, update.position.right_ankle_pitch)?;
-        self.set_joint_position(self.joint_handles.right_ankle_roll, update.position.right_ankle_roll)?;
+        self.set_joint_position(
+            self.joint_handles.right_hip_roll,
+            update.position.right_hip_roll,
+        )?;
+        self.set_joint_position(
+            self.joint_handles.right_hip_pitch,
+            update.position.right_hip_pitch,
+        )?;
+        self.set_joint_position(
+            self.joint_handles.right_knee_pitch,
+            update.position.right_knee_pitch,
+        )?;
+        self.set_joint_position(
+            self.joint_handles.right_ankle_pitch,
+            update.position.right_ankle_pitch,
+        )?;
+        self.set_joint_position(
+            self.joint_handles.right_ankle_roll,
+            update.position.right_ankle_roll,
+        )?;
 
         self.set_joint_stiffness(self.joint_handles.head_pitch, update.stiffness.head_pitch)?;
         self.set_joint_stiffness(self.joint_handles.head_yaw, update.stiffness.head_yaw)?;
 
-        self.set_joint_stiffness(self.joint_handles.left_shoulder_pitch, update.stiffness.left_shoulder_pitch)?;
-        self.set_joint_stiffness(self.joint_handles.left_shoulder_roll, update.stiffness.left_shoulder_roll)?;
-        self.set_joint_stiffness(self.joint_handles.left_elbow_yaw, update.stiffness.left_elbow_yaw)?;
-        self.set_joint_stiffness(self.joint_handles.left_elbow_roll, update.stiffness.left_elbow_roll)?;
-        self.set_joint_stiffness(self.joint_handles.left_wrist_yaw, update.stiffness.left_wrist_yaw)?;
-        self.set_joint_stiffness(self.joint_handles.left_hip_yaw_pitch, update.stiffness.left_hip_yaw_pitch)?;
-        self.set_joint_stiffness(self.joint_handles.left_hip_roll, update.stiffness.left_hip_roll)?;
-        self.set_joint_stiffness(self.joint_handles.left_hip_pitch, update.stiffness.left_hip_pitch)?;
-        self.set_joint_stiffness(self.joint_handles.left_knee_pitch, update.stiffness.left_knee_pitch)?;
-        self.set_joint_stiffness(self.joint_handles.left_ankle_pitch, update.stiffness.left_ankle_pitch)?;
-        self.set_joint_stiffness(self.joint_handles.left_ankle_roll, update.stiffness.left_ankle_roll)?;
+        self.set_joint_stiffness(
+            self.joint_handles.left_shoulder_pitch,
+            update.stiffness.left_shoulder_pitch,
+        )?;
+        self.set_joint_stiffness(
+            self.joint_handles.left_shoulder_roll,
+            update.stiffness.left_shoulder_roll,
+        )?;
+        self.set_joint_stiffness(
+            self.joint_handles.left_elbow_yaw,
+            update.stiffness.left_elbow_yaw,
+        )?;
+        self.set_joint_stiffness(
+            self.joint_handles.left_elbow_roll,
+            update.stiffness.left_elbow_roll,
+        )?;
+        self.set_joint_stiffness(
+            self.joint_handles.left_wrist_yaw,
+            update.stiffness.left_wrist_yaw,
+        )?;
+        self.set_joint_stiffness(
+            self.joint_handles.left_hip_yaw_pitch,
+            update.stiffness.left_hip_yaw_pitch,
+        )?;
+        self.set_joint_stiffness(
+            self.joint_handles.left_hip_roll,
+            update.stiffness.left_hip_roll,
+        )?;
+        self.set_joint_stiffness(
+            self.joint_handles.left_hip_pitch,
+            update.stiffness.left_hip_pitch,
+        )?;
+        self.set_joint_stiffness(
+            self.joint_handles.left_knee_pitch,
+            update.stiffness.left_knee_pitch,
+        )?;
+        self.set_joint_stiffness(
+            self.joint_handles.left_ankle_pitch,
+            update.stiffness.left_ankle_pitch,
+        )?;
+        self.set_joint_stiffness(
+            self.joint_handles.left_ankle_roll,
+            update.stiffness.left_ankle_roll,
+        )?;
 
-        self.set_joint_stiffness(self.joint_handles.right_shoulder_pitch, update.stiffness.right_shoulder_pitch)?;
-        self.set_joint_stiffness(self.joint_handles.right_shoulder_roll, update.stiffness.right_shoulder_roll)?;
-        self.set_joint_stiffness(self.joint_handles.right_elbow_yaw, update.stiffness.right_elbow_yaw)?;
-        self.set_joint_stiffness(self.joint_handles.right_elbow_roll, update.stiffness.right_elbow_roll)?;
-        self.set_joint_stiffness(self.joint_handles.right_wrist_yaw, update.stiffness.right_wrist_yaw)?;
+        self.set_joint_stiffness(
+            self.joint_handles.right_shoulder_pitch,
+            update.stiffness.right_shoulder_pitch,
+        )?;
+        self.set_joint_stiffness(
+            self.joint_handles.right_shoulder_roll,
+            update.stiffness.right_shoulder_roll,
+        )?;
+        self.set_joint_stiffness(
+            self.joint_handles.right_elbow_yaw,
+            update.stiffness.right_elbow_yaw,
+        )?;
+        self.set_joint_stiffness(
+            self.joint_handles.right_elbow_roll,
+            update.stiffness.right_elbow_roll,
+        )?;
+        self.set_joint_stiffness(
+            self.joint_handles.right_wrist_yaw,
+            update.stiffness.right_wrist_yaw,
+        )?;
         // self.set_joint_stiffness(self.joint_handles.right_hip_yaw_pitch, update.stiffness.left_hip_yaw_pitch)?;
-        self.set_joint_stiffness(self.joint_handles.right_hip_roll, update.stiffness.right_hip_roll)?;
-        self.set_joint_stiffness(self.joint_handles.right_hip_pitch, update.stiffness.right_hip_pitch)?;
-        self.set_joint_stiffness(self.joint_handles.right_knee_pitch, update.stiffness.right_knee_pitch)?;
-        self.set_joint_stiffness(self.joint_handles.right_ankle_pitch, update.stiffness.right_ankle_pitch)?;
-        self.set_joint_stiffness(self.joint_handles.right_ankle_roll, update.stiffness.right_ankle_roll)?;
+        self.set_joint_stiffness(
+            self.joint_handles.right_hip_roll,
+            update.stiffness.right_hip_roll,
+        )?;
+        self.set_joint_stiffness(
+            self.joint_handles.right_hip_pitch,
+            update.stiffness.right_hip_pitch,
+        )?;
+        self.set_joint_stiffness(
+            self.joint_handles.right_knee_pitch,
+            update.stiffness.right_knee_pitch,
+        )?;
+        self.set_joint_stiffness(
+            self.joint_handles.right_ankle_pitch,
+            update.stiffness.right_ankle_pitch,
+        )?;
+        self.set_joint_stiffness(
+            self.joint_handles.right_ankle_roll,
+            update.stiffness.right_ankle_roll,
+        )?;
 
-        self.client.to_owned().step(true).unwrap();
+        self.client.to_owned().step(false).unwrap();
         Ok(())
     }
 
@@ -138,15 +232,51 @@ impl CoppeliaBackend {
     fn set_joint_stiffness(&self, joint_handle: i64, target_stiffness: f32) -> Result<()> {
         self.sim
             .set_joint_target_force(joint_handle, target_stiffness as f64, None)
-            .map_err(| e | Error::CoppeliaSetValueError(e.show()))
+            .map_err(|e| Error::CoppeliaSetValueError(e.show()))
     }
+}
 
-    // TODO: make a function for getting the object
-    fn get_object(&self, path: impl Into<String>) -> Result<()> {
-        let path: String = path.into();
-        // TODO: also needs the error mapping (you can use Error::CoppeliaGetObjectError)
+fn get_object(sim: &Sim, path: impl Into<String>) -> Result<i64> {
+    let path: String = path.into();
+    let object = sim
+        .get_object(path, None)
+        .map_err(|e| Error::CoppeliaGetObjectError(e.show()));
 
+    return object;
+}
 
-        Ok(())
-    }
+fn get_joint_handles(sim: &Sim) -> Result<JointArray<i64>> {
+    let joint_handles: JointArray<i64> = JointArray {
+        head_yaw: get_object(&sim, "/NAO/HeadYaw")?,
+        head_pitch: get_object(&sim, "/NAO/HeadPitch")?,
+
+        left_shoulder_pitch: get_object(&sim, "/NAO/LShoulderPitch")?,
+        left_shoulder_roll: get_object(&sim, "/NAO/LShoulderRoll")?,
+        left_elbow_yaw: get_object(&sim, "/NAO/LElbowYaw")?,
+        left_elbow_roll: get_object(&sim, "/NAO/LElbowRoll")?,
+        left_wrist_yaw: get_object(&sim, "/NAO/LWristYaw")?,
+        left_hip_yaw_pitch: get_object(&sim, "/NAO/LHipYawPitch")?,
+        left_hip_roll: get_object(&sim, "/NAO/LHipRoll")?,
+        left_hip_pitch: get_object(&sim, "/NAO/LHipPitch")?,
+        left_knee_pitch: get_object(&sim, "/NAO/LKneePitch")?,
+        left_ankle_pitch: get_object(&sim, "/NAO/LAnklePitch")?,
+        left_ankle_roll: get_object(&sim, "/NAO/LAnkleRoll")?,
+
+        right_shoulder_pitch: get_object(&sim, "/NAO/RShoulderPitch")?,
+        right_shoulder_roll: get_object(&sim, "/NAO/RShoulderRoll")?,
+        right_elbow_yaw: get_object(&sim, "/NAO/RElbowYaw")?,
+        right_elbow_roll: get_object(&sim, "/NAO/RElbowRoll")?,
+        right_wrist_yaw: get_object(&sim, "/NAO/RWristYaw")?,
+        // right_hip_yaw_pitch: self.get_object("/NAO/RHipYawPitch")?; // heeft LoLa niet,
+        right_hip_roll: get_object(&sim, "/NAO/RHipRoll")?,
+        right_hip_pitch: get_object(&sim, "/NAO/RHipPitch")?,
+        right_knee_pitch: get_object(&sim, "/NAO/RKneePitch")?,
+        right_ankle_pitch: get_object(&sim, "/NAO/RAnklePitch")?,
+        right_ankle_roll: get_object(&sim, "/NAO/RAnkleRoll")?,
+
+        // handen heeft coppelia niet dus nog een keer wrist als placeholder
+        left_hand: get_object(&sim, "/NAO/LWristYaw")?,
+        right_hand: get_object(&sim, "/NAO/RWristYaw")?,
+    };
+    Ok(joint_handles)
 }
