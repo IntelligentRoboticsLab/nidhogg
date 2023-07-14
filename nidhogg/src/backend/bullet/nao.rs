b33c1a9a91667789b8bfc696805d4805b19003d1
Gijs de Jong <berichtaangijs@gmail.com>
2023-07-14 18:04:28 +0200

    Update nao.rs

--- nidhogg/src/backend/bullet/nao.rs
+++ nidhogg/src/backend/bullet/nao.rs
@@ -1,8 +1,9 @@
+use miette::bail;
 use rubullet::nalgebra::{Isometry3, Quaternion, Vector3};
-use rubullet::{BodyId, LoadModelFlags, PhysicsClient, UrdfOptions, JointInfo};
+use rubullet::{BodyId, JointInfo, LoadModelFlags, PhysicsClient, UrdfOptions};
 
-use crate::Result;
 use crate::types::JointArray;
+use crate::Result;
 
 #[derive(Debug)]
 pub struct BulletNao {
@@ -27,7 +28,7 @@ impl BulletNao {
 
         for i in 0..physics_client.get_num_joints(id) {
             let joint_info = physics_client.get_joint_info(id, i);
-            println!("{}: {:?}", joint_info.link_name, joint_info);
+            println!("{}", joint_info.link_name);
         }
 
         physics_client.create_constraint(
@@ -45,10 +46,48 @@ impl BulletNao {
     }
 }
 
-fn build_joint_link_array(physics_client: &mut PhysicsClient, body_id: BodyId) -> JointArray<JointInfo> {
+fn build_joint_link_array(
+    physics_client: &mut PhysicsClient,
+    body_id: BodyId,
+) -> Result<JointArray<WrappedJointInfo>> {
+    let mut builder = JointArray::<WrappedJointInfo>::builder();
     for i in 0..physics_client.get_num_joints(body_id) {
         let joint_info = physics_client.get_joint_info(body_id, i);
+        match joint_info.joint_name.as_str() {
+            "HeadPitch" => {
+                builder = builder.head_pitch(WrappedJointInfo(joint_info));
+            },
+            "HeadYaw" => {
+                builder = builder.head_yaw(WrappedJointInfo(joint_info));
+            },
+            _ => {}
+        }
+    }
 
-        
+    Ok(builder.build())
+}
+
+struct WrappedJointInfo(JointInfo);
+
+impl Default for WrappedJointInfo {
+    fn default() -> Self {
+        Self(JointInfo {
+            joint_index: Default::default(),
+            joint_name: Default::default(),
+            joint_type: rubullet::JointType::Fixed,
+            q_index: Default::default(),
+            u_index: Default::default(),
+            flags: Default::default(),
+            joint_damping: Default::default(),
+            joint_friction: Default::default(),
+            joint_lower_limit: Default::default(),
+            joint_upper_limit: Default::default(),
+            joint_max_force: Default::default(),
+            joint_max_velocity: Default::default(),
+            link_name: Default::default(),
+            joint_axis: Default::default(),
+            parent_frame_pose: Isometry3::identity(),
+            parent_index: Default::default(),
+        })
     }
 }
