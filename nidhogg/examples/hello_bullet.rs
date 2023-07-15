0c190b34d3a1661f4a36a09e4c171f4c89cbb6e2
Gijs de Jong <berichtaangijs@gmail.com>
2023-07-15 15:20:40 +0200

    implement joint dynamics

--- nidhogg/examples/hello_bullet.rs
+++ nidhogg/examples/hello_bullet.rs
@@ -1,13 +1,43 @@
 //! An introduction to the usage of RuBullet.
-use std::{thread, time::Duration};
-use nidhogg::{Result, NaoBackend};
 use nidhogg::backend::BulletBackend;
+use nidhogg::types::JointArray;
+use nidhogg::{NaoBackend, NaoControlMessage, Result};
+use std::{thread, time::Duration};
 
 fn main() -> Result<()> {
     let mut bullet = BulletBackend::connect()?;
 
-    for _ in 0..10000 {
-        bullet.physics_client.step_simulation()?;
+    let stiffness = 1.0;
+    for t in 0..10000 {
+        bullet.send_control_msg(
+            NaoControlMessage::builder()
+                .position(
+                    JointArray::<f32>::builder()
+                        .left_ankle_pitch(-std::f32::consts::FRAC_PI_8)
+                        .right_ankle_pitch(-std::f32::consts::FRAC_PI_8)
+                        .left_knee_pitch(std::f32::consts::FRAC_PI_4)
+                        .right_knee_pitch(std::f32::consts::FRAC_PI_4)
+                        .left_hip_pitch(-std::f32::consts::FRAC_PI_6)
+                        .right_hip_pitch(-std::f32::consts::FRAC_PI_6)
+                        .head_yaw(1.5333 * f32::sin(0.01 * (t as f32)))
+                        .build(),
+                )
+                .stiffness(
+                    JointArray::<f32>::builder()
+                        .left_hip_pitch(stiffness)
+                        .right_hip_pitch(stiffness)
+                        .left_hip_roll(stiffness)
+                        .right_hip_roll(stiffness)
+                        .left_ankle_pitch(stiffness)
+                        .right_ankle_pitch(stiffness)
+                        .left_knee_pitch(stiffness)
+                        .right_knee_pitch(stiffness)
+                        .left_hip_yaw_pitch(stiffness)
+                        .head_yaw(stiffness)
+                        .build(),
+                )
+                .build(),
+        )?;
         thread::sleep(Duration::from_millis(1));
     }
 
--- nidhogg/src/backend/bullet/environment.rs
+++ nidhogg/src/backend/bullet/environment.rs
@@ -1,25 +1,20 @@
 use crate::Result;
-use rubullet::nalgebra::{Isometry3, Vector3};
+use rubullet::nalgebra::Vector3;
 use rubullet::{BodyId, PhysicsClient};
 
-use super::nao::BulletNao;
 
 #[derive(Debug)]
 pub struct NaoBulletEnvironment {
     pub plane_id: BodyId,
-    pub nao: BulletNao,
 }
 
 impl NaoBulletEnvironment {
     pub fn create(physics_client: &mut PhysicsClient) -> Result<Self> {
         physics_client.set_additional_search_path("../../bullet_data")?;
-        physics_client.set_gravity(Vector3::new(0.0, 0.0, -10.0));
+        physics_client.set_gravity(Vector3::new(0.0, 0.0, -9.81));
 
         let plane_id = physics_client.load_urdf("plane.urdf", None)?;
 
-        let start_position = Isometry3::translation(0.0, 0.0, 1.0);
-        let nao = BulletNao::create(physics_client, start_position)?;
-
-        Ok(NaoBulletEnvironment { plane_id, nao })
+        Ok(NaoBulletEnvironment { plane_id })
     }
 }
