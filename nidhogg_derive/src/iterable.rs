9725ca01510c7530ef9782e56b1bba47adad5775
jakob <jakob.j.jeep@gmail.com>
2023-07-30 19:52:15 +0200

    added from iter

--- nidhogg_derive/src/iterable.rs
+++ nidhogg_derive/src/iterable.rs
@@ -14,15 +14,20 @@ pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     match generics.params.first() {
         Some(GenericParam::Type(TypeParam {
             ident: field_type, ..
-        })) => match parse_fields(data) {
-            Ok(fields) => {
-                let impl_to_vec = impl_to_vec(&struct_name, &generics, &fields);
+        })) => match parse_data(data) {
+            Ok((fields_clone, fields)) => {
+                let impl_to_vec = impl_to_vec(&struct_name, &generics, &fields_clone);
                 let impl_into_iterator =
-                    impl_into_iterator(&struct_name, &generics, &fields, field_type);
+                    impl_into_iterator(&struct_name, &generics, &fields_clone, field_type);
+                let impl_from_iterator =
+                    impl_from_iterator(&struct_name, &generics, &fields, field_type);
 
                 quote! {
                     #impl_to_vec
+
                     #impl_into_iterator
+
+                    #impl_from_iterator
                 }
             }
             Err(err) => err,
@@ -33,7 +38,7 @@ pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     .into()
 }
 
-fn parse_fields(data: Data) -> Result<Vec<TokenStream>, TokenStream> {
+fn parse_data(data: Data) -> Result<(Vec<TokenStream>, Vec<TokenStream>), TokenStream> {
     let fields = match data {
         Data::Struct(data_struct) => match data_struct.fields {
             Fields::Named(fields_named) => fields_named.named,
@@ -51,25 +56,39 @@ fn parse_fields(data: Data) -> Result<Vec<TokenStream>, TokenStream> {
     };
 
     if fields.is_empty() {
-        panic!("Must contain at least one field!");
+        return Err(
+            syn::Error::new_spanned(fields, "Must contain at least one field!").to_compile_error(),
+        );
     }
 
-    Ok(fields
-        .iter()
-        .map(|Field { ident, .. }| {
-            quote! { self.#ident.clone() }
-        })
-        .collect())
+    Ok((
+        fields
+            .iter()
+            .map(|Field { ident, .. }| {
+                quote! { self.#ident.clone() }
+            })
+            .collect(),
+        fields
+            .iter()
+            .map(|Field { ident, .. }| {
+                quote! { #ident }
+            })
+            .collect(),
+    ))
 }
 
-fn impl_to_vec(struct_name: &Ident, generics: &Generics, fields: &Vec<TokenStream>) -> TokenStream {
+fn impl_to_vec(
+    struct_name: &Ident,
+    generics: &Generics,
+    self_fields_with_clone: &Vec<TokenStream>,
+) -> TokenStream {
     let (_, ty_generics, _) = generics.split_for_impl();
     let impl_generics = generic_type_params_with_clone(generics);
 
     quote! {
         impl <#(#impl_generics)*> #struct_name #ty_generics {
              pub fn to_vec(&self) -> std::vec::Vec #ty_generics {
-                 vec![#(#fields), *]
+                 vec![#(#self_fields_with_clone), *]
              }
         }
     }
@@ -78,7 +97,7 @@ fn impl_to_vec(struct_name: &Ident, generics: &Generics, fields: &Vec<TokenStrea
 fn impl_into_iterator(
     struct_name: &Ident,
     generics: &Generics,
-    fields: &Vec<TokenStream>,
+    self_fields_with_clone: &Vec<TokenStream>,
     field_type: &Ident,
 ) -> TokenStream {
     let (_, ty_generics, _) = generics.split_for_impl();
@@ -90,7 +109,36 @@ fn impl_into_iterator(
             type IntoIter = std::vec::IntoIter<Self::Item>;
 
             fn into_iter(self) -> std::vec::IntoIter<#field_type> {
-                vec![#(#fields), *].into_iter()
+                vec![#(#self_fields_with_clone), *].into_iter()
+            }
+        }
+    }
+}
+
+fn impl_from_iterator(
+    struct_name: &Ident,
+    generics: &Generics,
+    fields: &Vec<TokenStream>,
+    field_type: &Ident,
+) -> TokenStream {
+    let (impl_generics, ty_generics, _) = generics.split_for_impl();
+    let l = fields.len();
+
+    quote! {
+        impl #impl_generics FromIterator<#field_type> for #struct_name #ty_generics {
+            fn from_iter<I: IntoIterator<Item=#field_type>>(iter: I) -> Self {
+                let mut t: Vec<#field_type> = Vec::new();
+
+                for item in iter {
+                    t.push(item);
+                }
+
+                let Ok([#(#fields), *]): Result<[#field_type; #l], Vec<#field_type>> = t.try_into() else {
+                    panic!("Not enough values in iterator");
+                };
+                Self {
+                    #(#fields), *
+                }
             }
         }
     }
--- nidhogg_derive/tests/iterable_test.rs
+++ nidhogg_derive/tests/iterable_test.rs
@@ -10,15 +10,39 @@ mod iterable_tests {
     ///```
     #[derive(Debug, Iterable)]
     pub struct TestStruct<T> {
-        pub test_field: T,
+        pub test_field_1: T,
+        pub test_field_2: T,
     }
 
     #[test]
-    fn it_works() {
-        let t = TestStruct { test_field: 5 };
+    fn into_iter_works() {
+        let t = TestStruct {
+            test_field_1: 5,
+            test_field_2: -5,
+        };
 
-        for elem in t.into_iter() {
-            assert_eq!(elem, 5)
+        for (a, b) in t.into_iter().zip(vec![5, -5]) {
+            assert_eq!(a, b);
+        }
+    }
+
+    #[test]
+    fn from_iter_works() {
+        let t0 = TestStruct {
+            test_field_1: 5,
+            test_field_2: -5,
+        };
+
+        let t1 = TestStruct {
+            test_field_1: 11,
+            test_field_2: -3,
+        };
+
+        let t2: TestStruct<i32> =
+            t0.into_iter().zip(t1.into_iter()).map(|(a, b)| (a + b) / 2).collect();
+
+        for (a, b) in t2.into_iter().zip(vec![8, -4]) {
+            assert_eq!(a, b);
         }
     }
 }
